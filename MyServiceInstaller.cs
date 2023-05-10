fb3b7e97d68b96079443a57e3b53513bd42e2c2e
Aleksa Jankovic <ajanko@microsoft.com>
2023-05-10 13:00:30 +0200

    squash

---
+++ MyServiceInstaller.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+
+[RunInstaller(true)]
+public class MyServiceInstaller : Installer
+{
+    public MyServiceInstaller()
+    {
+        var serviceInstaller = new System.ServiceProcess.ServiceInstaller();
+        var serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+
+        // Configure the service installer
+        serviceInstaller.ServiceName = "PowerOutageNotifier";
+        serviceInstaller.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+
+        // Configure the service process installer
+        serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+
+        // Add the installers to the installer collection
+        Installers.Add(serviceInstaller);
+        Installers.Add(serviceProcessInstaller);
+    }
+}
---
+++ Service1.cs
@@ -0,0 +1,112 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace PowerOutageNotifier
+{
+    public partial class Service1 : ServiceBase
+    {
+        private static readonly string telegramBotToken = ConfigReader.ReadBotToken();
+
+        public static readonly List<UserData> userDataList = ConfigReader.ReadUserData();
+
+        // URLs of the web page to scrape
+        private static readonly List<string> urls = new List<string>
+        {
+            "http://www.epsdistribucija.rs/planirana-iskljucenja-beograd/Dan_0_Iskljucenja.htm",
+            "http://www.epsdistribucija.rs/planirana-iskljucenja-beograd/Dan_1_Iskljucenja.htm",
+            "http://www.epsdistribucija.rs/planirana-iskljucenja-beograd/Dan_2_Iskljucenja.htm",
+            "http://www.epsdistribucija.rs/planirana-iskljucenja-beograd/Dan_3_Iskljucenja.htm",
+        };
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            SendMessageAsync(userDataList.First().ChatId, "Service running").GetAwaiter().GetResult();
+
+            while (true)
+            {
+                try
+                {
+                    CheckAndNotifyPowerOutage();
+                    Thread.Sleep(TimeSpan.FromHours(1));
+                }
+                catch (Exception)
+                {
+                        // just continue
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+            SendMessageAsync(userDataList.First().ChatId, "Service stopping").GetAwaiter().GetResult();
+        }
+
+        private static async Task SendMessageAsync(long chatId, string message)
+        {
+            TelegramBotClient botClient = new TelegramBotClient(telegramBotToken);
+            await botClient.SendTextMessageAsync(chatId, message);
+        }
+
+        public static void CheckAndNotifyPowerOutage()
+        {
+            foreach (string url in urls)
+            {
+                // Create a new HtmlWeb instance
+                HtmlWeb web = new HtmlWeb();
+
+                // Load the HTML document from the specified URL
+                HtmlDocument document = web.Load(url);
+
+                // Find the table rows in the document
+                HtmlNodeCollection rows = document.DocumentNode.SelectNodes("//table//tr");
+
+                // Iterate through the rows
+                foreach (HtmlNode row in rows)
+                {
+                    // Find the cells in the current row
+                    HtmlNodeCollection cells = row.SelectNodes("td");
+
+                    // Check if the row has the correct number of cells
+                    if (cells != null && cells.Count >= 3)
+                    {
+                        // Get the district name from the first cell
+                        string district = cells[0].InnerText.Trim();
+
+                        // Get the street name from the second cell
+                        string streets = cells[2].InnerText.Trim();
+
+                        foreach (var user in userDataList)
+                        {
+                            // Check if the street name occurs in the same row as the correct district name
+                            if (district == user.DistrictName && streets.Contains(user.StreetName))
+                            {
+                                Console.WriteLine($"Outage detected. {user.FriendlyName}, {user.DistrictName}, {user.StreetName}, {user.ChatId}");
+
+                                int daysLeftUntilOutage = urls.IndexOf(url);
+
+                                SendMessageAsync(user.ChatId, $"Power outage will occurr in {daysLeftUntilOutage} days in {user.DistrictName}, {user.StreetName}.")
+                                    .GetAwaiter().GetResult();
+
+                                break; // You can exit the loop if the match is found
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
