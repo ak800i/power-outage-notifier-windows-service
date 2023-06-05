67a3b1fcd7dca69ab066cbe4ea5646b37e943c8c
Aleksa Jankovic <ajanko@microsoft.com>
2023-06-05 17:03:02 +0200

    added installation file

---
+++ install.bat
@@ -0,0 +1,2 @@
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" /u bin\Release\PowerOutageNotifier.exe
+"C:\Windows\Microsoft.NET\Framework64\v4.0.30319\InstallUtil.exe" bin\Release\PowerOutageNotifier.exe
